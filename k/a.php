0587add547474ebae02b0b0df78d34264dca1052
Jutaporn <66010914004@msu.ac.th>
2026-02-18 17:01:24 +0700

    a

---
+++ k/a.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>งาน K </title>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+        }
+        .btn {
+            padding: 10px 20px;
+            font-size: 16px;
+            border: none;
+            cursor: pointer;
+            margin: 10px;
+        }
+        .green {
+            background-color: green;
+            color: white;
+        }
+        .orange {
+            background-color: orange;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+
+<h1>งาน K 66010914004 จุฑาภรณ์ สมจันทร์ </h1>
+
+
+<button class="btn green" onclick="document.getElementById('img1').style.display='block'">
+    สีเขียว
+</button>
+
+<button class="btn orange" onclick="document.getElementById('img2').style.display='block'">
+    สีเหลือง
+</button>
+
+<br><br>
+
+<img id="img1" src="1.jpg" width="300" style="display:none;">
+<img id="img2" src="2.jpg" width="300" style="display:none;">
+
+</body>
+</html>
