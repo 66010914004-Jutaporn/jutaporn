e221cca29e5a04a3c483fc51f8546990213ff87b
Jutaporn <66010914004@msu.ac.th>
2026-02-18 15:14:36 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>งาน j -- 66010914004จุฑาภรณ์ สจันทร์ (ฝน)</title>
+</head>
+<body>
+<h1>งาน j -- 66010914004จุฑาภรณ์ สจันทร์ (ฝน)
+</h1>
+<img src= "../../../../Users/Administrator/Downloads/1.jpg"width="200" >
+    
+</body>
+</html>
